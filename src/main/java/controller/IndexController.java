2977499535e9db1223b5878a3269614fec38d9fe
SkyWorkerCK <icantouchsky@foxmail.com>
2017-11-14 14:36:29 +0800

    SSM
    This is a simple SSM framework for fresh man to start .

---
+++ src/main/java/controller/IndexController.java
@@ -0,0 +1,14 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+	@RequestMapping(value="hello.html")
+	public String helloWorld(){
+		return "index";
+	}
+	
+}
